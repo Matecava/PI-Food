e95c052ff4d2a336e344e62b86290882c513a167
Matecava <matecava04@gmail.com>
2023-05-09 17:45:47 -0300

    deploy rw fe

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pi-food-production-03f3.up.railway.app";
 
 ReactDOM.render(
   <Provider store={store}>
