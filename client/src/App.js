b7c1fded516fde3ba49b2e159bfd1ab1c2e3ffee
Matecava <matecava04@gmail.com>
2023-05-09 18:31:47 -0300

    app change

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Home from './components/Home';
 import RecipeDetails from './components/RecipeDetails';
@@ -7,12 +7,14 @@ import CreateRecipe from './components/CreateRecipe'
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<LandingPage/>}/>
+    <BrowserRouter>
+    <Switch>
+    <Route exact path='/' element={<LandingPage/>}/>
       <Route path='/home' element={<Home/>}/>
       <Route path='/recipe/:idRecipe' element={<RecipeDetails/>}/>
       <Route path='/recipecreate' element={<CreateRecipe/>}/>
-    </Routes>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
